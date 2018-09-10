fb783910ae4fe5ea5deee7c48371458f9655c483
etienne-napoleone <etienne@tomochain.com>
2018-09-10 17:31:53 +0700

    chore: bump version

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -31,7 +31,7 @@ def tmn():
 
 
 def test_version(runner, tmn):
-    version = '0.0.9'
+    version = '0.1.0'
     result = runner.invoke(tmn.main, ['--version'])
     assert result.output[-6:-1] == version
     assert package.__version__ == version
