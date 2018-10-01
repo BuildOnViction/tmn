4d6262884b41ef1a1f76bccf5b0049ef4085bc03
etienne-napoleone <etienne@tomochain.com>
2018-10-01 14:28:05 +0700

    fix: fix test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -184,7 +184,7 @@ def test_command_start_ignore(runner, tmn):
     ])
     result = runner.invoke(tmn.main, ['start', '--name', 'test'])
     lines = result.output.splitlines()
-    assert '! warning: masternode test is already configured' in lines
+    assert '! warning: masternode test1 is already configured' in lines
     _clean(tmn)
 
 
